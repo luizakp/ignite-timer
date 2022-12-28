1bdc7d0ef26ef902616b3257a46ccc8271844f62
luizakp <luiza.puccinelli06@gmail.com>
2022-12-28 17:09:35 +0100

    add styledComponents

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import { ThemeProvider } from "styled-components";
+
+import { GlobalStyle } from "./styles/global";
+import { defaultTheme } from "./styles/themes/default";
+
 export function App() {
   return (
-    <div>
+    <ThemeProvider theme={defaultTheme}>
       <h1>hello</h1>
-    </div>
+      <GlobalStyle />
+    </ThemeProvider>
   );
 }
